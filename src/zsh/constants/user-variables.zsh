823d1586c339c8c182c8150a85fcc4ec5794c2e4
Frederick Engelhardt <fre@virtualize.dev>
2024-03-22 19:56:10 -0700

    add variable for zshrc file prefix that backs up the previous config

--- src/zsh/constants/user-variables.zsh
+++ src/zsh/constants/user-variables.zsh
@@ -1,7 +1,7 @@
 export GIT_GLOBAL_USER=""
 export GIT_GLOBAL_USER_EMAIL=""
 export GIT_GLOBAL_USER_COMPANY_EMAIL=""
-
+export ZSH_BACKUP_FILE_PREFIX_NAME=".zshrc_pre_modular_platform_config"
 # The overrideFile is used to override any local variables
 overrideFile="$MPC_ZSH_CONSTANTS/user-variables.local.zsh"
 [[ -f $overrideFile ]] && source $overrideFile
